24f6851aeb4b52d959ffd427ceda439e81b809e6
Martin Picard <martin.picard@emc.com>
2018-01-19 09:53:57 +0000

    Clean up

--- index.js
+++ index.js
@@ -1,80 +1,71 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const { join } = require('path');
 const chalk = require('chalk');
-const minimist = require('minimist');
-const simpleGit = require('simple-git');
-const semver = require('semver');
 const jsonfile = require('jsonfile');
-
-
-const argv = minimist(process.argv.slice(2));
-const path = argv['path'] ? argv['path'] : 'package.json';
-const file = `${process.cwd()}/${path}`;
-var package = jsonfile.readFileSync(file);
-const git = simpleGit(process.cwd())
-  .outputHandler(function (cmd, stdout, stderr) {
-    stdout.pipe(process.stdout);
-    stderr.pipe(process.stderr);
-  });
+const minimist = require('minimist');
+const semver = require('semver');
+const simpleGit = require('simple-git');
 
 function error(msg) {
   console.log(chalk.red(`Error: ${msg}`));
+  process.exit(1);
 }
 
 function log(msg) {
   console.log(chalk.yellow(`${msg}`));
 }
 
-function missingType() {
-  error('Missing semantic type\n');
-  log('Usage: bump-tag-version major|minor|patch');
-  process.exit(1);
-}
+(function () {
+  // try {
+  const git = simpleGit().outputHandler(function (cmd, stdout, stderr) {
+    stdout.pipe(process.stdout);
+    stderr.pipe(process.stderr);
+  });
 
+  const argv = minimist(process.argv.slice(2));
+  const filename = argv['f'] || argv['file'] || 'package.json';
+  const packagePath = join(process.cwd(), filename);
+  const package = jsonfile.readFileSync(packagePath);
 
-function bump(type, label) {
-  const oldVersion = package.version;
-  const newVersion = label ?
-    semver.inc(oldVersion, type, label) :
-    semver.inc(oldVersion, type);
+  function bump(type, label) {
+    const oldVersion = package.version;
+    const newVersion = label ?
+      semver.inc(oldVersion, type, label) :
+      semver.inc(oldVersion, type);
 
-  // package.json
-  package.version = newVersion;
-  jsonfile.writeFileSync(file, package, { spaces: 2 });
+    // package.json
+    package.version = newVersion;
+    jsonfile.writeFileSync(packagePath, package, { spaces: 2 });
 
-  // git tag
-  git.addTag(newVersion);
-  git.pushTags();
+    // tags
+    git.tag([newVersion]);
+    git.pushTags('origin');
 
-  console.log(chalk.green(`v${oldVersion} => v${newVersion}`));
-}
-
-function init() {
-  try {
-    if (package == null) {
-      error('no package.json found');
-      process.exit(1);
-    }
-    const typeArg = argv['_'];
-    switch (typeArg[0]) {
-      case 'major':
-      case 'minor':
-      case 'patch':
-        bump(typeArg[0])
-        break;
-      case 'prerelease':
-      case 'prepatch':
-        bump(typeArg[0], typeArg[1]);
-        break;
-      default:
-        bump('patch');
-        break;
-    }
-  } catch (e) {
-    error(e.stack);
-    process.exit(1);
+    console.log(chalk.green(`v${oldVersion} => v${newVersion}`));
   }
-  process.exit(0);
-}
 
-init();
+  if (package == null) {
+    error('no package.json found, use -f or change directories');
+  }
+
+  const type = argv['_'];
+  switch (type[0]) {
+    case 'major':
+    case 'minor':
+    case 'patch':
+      bump(type[0]);
+      break;
+    case 'prerelease':
+    case 'prepatch':
+      bump(type[0]);
+      break;
+    default:
+      bump('patch');
+      break;
+  }
+  // } catch (e) {
+  //   error(e.stack);
+  // }
+  process.exit(0);
+})();
